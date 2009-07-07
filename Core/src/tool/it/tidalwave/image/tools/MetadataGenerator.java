0c25028f7337d932b61944116ef6f28ec01e4a32
fabriziogiudici <fabriziogiudici@localhost>
2009-07-07 11:55:08 +0200

    [svn r1047] Sources for metadata generator moved here.

---
+++ Core/src/tool/it/tidalwave/image/tools/MetadataGenerator.java
@@ -0,0 +1,67 @@
+package it.tidalwave.image.tools;
+
+import java.util.Date;
+import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+import org.antlr.runtime.ANTLRReaderStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+import it.tidalwave.image.tools.grammar.TIFFLexer;
+import it.tidalwave.image.tools.grammar.TIFFParser;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class MetadataGenerator 
+  {
+    private List<TIFFRecord> records;
+    
+    public static void main (final String ... args) 
+      throws Exception
+      {
+        final ClassLoader loader = MetadataGenerator.class.getClassLoader();
+        final InputStreamReader input = new InputStreamReader(loader.getResourceAsStream("resources/" + args[0]));
+        final MetadataGenerator exifGenerator = new MetadataGenerator(input);
+        final String className = args[1];
+        final String path = args[2];
+        final File file = new File(String.format("%s/%s.java", path, className));
+        file.getParentFile().mkdirs();
+        final PrintWriter output = new PrintWriter(new FileWriter(file));
+        exifGenerator.generate(output, className);
+        output.close();
+      }
+
+    public MetadataGenerator (final Reader input) 
+      throws IOException, RecognitionException
+      {
+        final ANTLRReaderStream reader = new ANTLRReaderStream(input);
+        final TIFFLexer lexer = new TIFFLexer(reader);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final TIFFParser parser = new TIFFParser(tokens);
+        records = parser.prog();
+      }
+
+    private StringTemplateGroup getGroup() 
+      {
+        InputStream templateStream = this.getClass().getClassLoader().getResourceAsStream("resources/MetadataGenerator.stg");
+        Reader templateReader = new InputStreamReader(templateStream);
+        return new StringTemplateGroup(templateReader);
+      }  
+
+    public void generate (final Writer output, final String className) 
+      throws IOException
+      {
+        final StringTemplateGroup group = getGroup();
+        final StringTemplate template = group.getInstanceOf("generator");
+        template.setAttribute("creation_date", new Date(0));
+        template.setAttribute("class_name", className);
+        template.setAttribute("records", records);
+        output.write(template.toString());
+      }
+  }
---
+++ Core/src/tool/it/tidalwave/image/tools/TIFFRecord.java
@@ -0,0 +1,116 @@
+package it.tidalwave.image.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class TIFFRecord 
+  {
+    private String id;
+    private String tagName;
+    private String type;
+    private List<TIFFEnumValueName> enums = new ArrayList<TIFFEnumValueName>();
+    private static ResourceBundle javaTypeDict = ResourceBundle.getBundle("resources/TIFFRecord_JavaType");
+    private static final String UNDEFINED = "undefined";
+
+    public TIFFRecord (final String id, final String tagName, final String type, final List<TIFFEnumValueName> enums) 
+      {
+        this.id = id;
+        this.tagName = tagName;
+        this.type = type.toLowerCase();
+        
+        if (enums != null && !enums.isEmpty()) 
+          {
+            this.enums.addAll(enums);
+          }  
+      }
+
+    public int getId() 
+      {
+        try 
+          {
+            return Integer.parseInt(id);
+          }
+        catch (NumberFormatException e) 
+          {
+            return -1;
+          }
+      }
+
+    public String getTagName() 
+      {
+        return tagName;
+      }
+
+    public String getType() 
+      {
+        if (type.equals(UNDEFINED)) 
+          {
+            return "byte";
+          }
+        
+        return type;
+      }
+
+    public String getJavaType() 
+      {
+        if (this.isHasEnums()) 
+          {
+            return this.getCapitalizedTagName();
+          }
+
+        String type = getType();
+        
+        try 
+          {
+            return javaTypeDict.getString(type);
+          } 
+        catch (MissingResourceException e) 
+          {
+            return type;
+          }
+      }
+
+    public String getJavaTypeToString() 
+      {
+        String result = "get" + this.getCapitalizedTagName() + "()";
+        
+        if (this.getJavaType().endsWith("[]")) 
+          {
+            result = "toString(" + result + ")";
+          }
+        
+        return result;
+      }
+
+    public String getUncapitalizedTagName() 
+      {
+        return Utils.uncapitalized(getStrippedTagName());
+      }
+
+    public String getCapitalizedTagName() 
+      {
+        return Utils.capitalized(getStrippedTagName());
+      }  
+
+    public String getConstTagName() 
+      {
+        return Utils.strip(tagName.toUpperCase().replace(' ', '_'));
+      }
+
+    public boolean isHasEnums() 
+      {
+        return !enums.isEmpty();
+      }
+
+    public List<TIFFEnumValueName> getEnums() 
+      {
+        return new ArrayList<TIFFEnumValueName>(enums);
+      }
+
+    private String getStrippedTagName() 
+      {
+        return Utils.strip(tagName).replace('/', '_').replace('(', '_').replace(')', '_');
+      }
+  }
