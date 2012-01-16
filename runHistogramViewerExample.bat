06742e1ef023fc7204b3e6b36e72abb92e4e35b5
fabriziogiudici <fabrizio dot giudici at tidalwave dot it@localhost>
2012-01-16 21:17:25 +0100

    Fixed example launchers.

--- runHistogramViewerExample.bat
+++ runHistogramViewerExample.bat
@@ -23,4 +23,4 @@
 
 @REM $Id; runHistogramViewerExample.bat 450 2007-02-02 12;02;52Z fabriziogiudici $
 
-mvn -f Examples/HistogramViewerExample/pom.xml -Dmaven.test.skip=true install exec:java -Dexec.mainClass=it.tidalwave.mistral.example.histogramviewer.Main
+mvn -f Examples/HistogramViewerExample/pom.xml -Dmaven.test.skip=true install exec:java 
--- runViewerExample.bat
+++ runViewerExample.bat
@@ -23,4 +23,4 @@
 
 @REM #$Id; runViewerExample.bat 450 2007-02-02 12;02;52Z fabriziogiudici $
 
-mvn -f Examples/ViewerExample/pom.xml -Dmaven.test.skip=true install exec:java -Dexec.mainClass=it.tidalwave.mistral.example.viewer.Main
+mvn -f modules/Examples/ViewerExample/pom.xml -Dmaven.test.skip=true install exec:java 
