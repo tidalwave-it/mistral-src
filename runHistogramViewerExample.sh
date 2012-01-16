7df4711dda878317f2843f39d8823ff2c36fe55c
fabriziogiudici <fabrizio dot giudici at tidalwave dot it@localhost>
2012-01-16 17:03:42 +0100

    ...

--- runHistogramViewerExample.sh
+++ runHistogramViewerExample.sh
@@ -22,6 +22,6 @@
 #
 #
 
-#$Id: runHistogramViewerExample.sh 1038 2009-07-06 20:21:57Z fabriziogiudici $
+#$Id$
 
 mvn -f Examples/HistogramViewerExample/pom.xml -Dmaven.test.skip=true install exec:java -Dexec.mainClass=it.tidalwave.mistral.example.histogramviewer.Main
--- runViewerExample.sh
+++ runViewerExample.sh
@@ -22,6 +22,6 @@
 #
 #
 
-#$Id: runViewerExample.sh 1038 2009-07-06 20:21:57Z fabriziogiudici $
+#$Id$
 
 mvn -f Examples/ViewerExample/pom.xml -Dmaven.test.skip=true install exec:java -Dexec.mainClass=it.tidalwave.mistral.example.viewer.Main
