9f0c01ea28d61d0e032e625c0e8cc141ead94e83
fabriziogiudici <fabrizio dot giudici at tidalwave dot it@localhost>
2012-01-16 17:03:33 +0100

    Clean up.

--- modules/Operations/src/main/java/it/tidalwave/image/java2d/ConvertColorProfileJ2DOp.java
+++ modules/Operations/src/main/java/it/tidalwave/image/java2d/ConvertColorProfileJ2DOp.java
@@ -22,7 +22,7 @@
  **********************************************************************************************************************/
 package it.tidalwave.image.java2d;
 
-import java.util.logging.Logger;
+import javax.annotation.Nonnull;
 import java.awt.RenderingHints;
 import java.awt.color.ColorSpace;
 import java.awt.color.ICC_ColorSpace;
@@ -38,62 +38,60 @@ import it.tidalwave.image.EditableImage;
 import it.tidalwave.image.ImageUtils;
 import it.tidalwave.image.op.ConvertColorProfileOp;
 import it.tidalwave.image.op.OperationImplementation;
+import lombok.extern.slf4j.Slf4j;
 
-
-/*******************************************************************************
+/***********************************************************************************************************************
  *
  * @author  Fabrizio Giudici
  * @version $Id$
  *
- ******************************************************************************/
+ **********************************************************************************************************************/
+@Slf4j
 public class ConvertColorProfileJ2DOp extends OperationImplementation<ConvertColorProfileOp, BufferedImage>
   {
-    private static final String CLASS = ConvertColorProfileJ2DOp.class.getName();
-    private static final Logger logger = Logger.getLogger(CLASS);
-
-    /*******************************************************************************
+    /*******************************************************************************************************************
      *
-     * @inheritDoc
+     * {@inheritDoc}
      *
-     ******************************************************************************/
+     ******************************************************************************************************************/
 
     // FIXME: pass another parameter to specify the quality of the operation
-    protected BufferedImage execute (ConvertColorProfileOp operation, final EditableImage image, BufferedImage bufferedImage)
+    @Nonnull
+    protected BufferedImage execute (final @Nonnull ConvertColorProfileOp operation,
+                                     final @Nonnull EditableImage image, 
+                                     final @Nonnull BufferedImage bufferedImage)
       {
-        ICC_Profile colorProfile = operation.getICCProfile();
-        logger.fine("convertColorProfile(" + ImageUtils.getICCProfileName(colorProfile) + ")");
-        Java2DUtils.logImage(logger, ">>>> source bufferedImage", bufferedImage);
+        final ICC_Profile colorProfile = operation.getICCProfile();
+        log.debug("convertColorProfile({})", ImageUtils.getICCProfileName(colorProfile));
+        Java2DUtils.logImage(log, ">>>> source bufferedImage", bufferedImage);
 
-        ICC_Profile sourceProfile = ImageUtils.getICCProfile(bufferedImage);
-        String sourceProfileName = ImageUtils.getICCProfileName(sourceProfile);
-        logger.finer(">>>> Converting profile from " + sourceProfileName + " to "
-            + ImageUtils.getICCProfileName(colorProfile));
+        final ICC_Profile sourceProfile = ImageUtils.getICCProfile(bufferedImage);
+        final String sourceProfileName = ImageUtils.getICCProfileName(sourceProfile);
+        log.debug(">>>> Converting profile from {}  to {}", sourceProfileName, ImageUtils.getICCProfileName(colorProfile));
 
-        RenderingHints hints = null; // FIXME
-        ColorConvertOp ccOp = new ColorConvertOp(new ICC_Profile[] { colorProfile }, hints);
+        final RenderingHints hints = null; // FIXME
+        final ColorConvertOp ccOp = new ColorConvertOp(new ICC_Profile[] { colorProfile }, hints);
 
         // Strategy 1: it would be the best, but reduces depth to 8 bit and converts to PixelInterleavedRaster, which is SLOW!
         //        image = ccOp.filter(image, null); // - produce PixelInt BYTE
         // Strategy 2: create a dest image which is already an 8-bit packed raster
         // FIXME: this reduces the depth to 8 bit, which we don't want!
-        logger.warning(">>>> **** WARNING: convertColorProfile() is reducing depth to 8 bit!");
+        log.warn(">>>> **** WARNING: convertColorProfile() is reducing depth to 8 bit!");
 
-        WritableRaster raster = Raster.createPackedRaster(DataBuffer.TYPE_INT, bufferedImage.getWidth(),
-                bufferedImage.getHeight(), 3, 8, null);
-        ColorSpace colorSpace = new ICC_ColorSpace(colorProfile);
-        ColorModel colorModel = new DirectColorModel(colorSpace, 24, 0x00ff0000, 0x0000ff00, 0x000000ff, 0, false,
-                DataBuffer.TYPE_INT);
+        final WritableRaster raster = Raster.createPackedRaster(DataBuffer.TYPE_INT, bufferedImage.getWidth(), bufferedImage.getHeight(), 3, 8, null);
+        final ColorSpace colorSpace = new ICC_ColorSpace(colorProfile);
+        final ColorModel colorModel = new DirectColorModel(colorSpace, 24, 0x00ff0000, 0x0000ff00, 0x000000ff, 0, false, DataBuffer.TYPE_INT);
 
-        BufferedImage image2 = new BufferedImage(colorModel, raster, false, Java2DUtils.getProperties(bufferedImage));
-        bufferedImage = ccOp.filter(bufferedImage, image2);
+        final BufferedImage image2 = new BufferedImage(colorModel, raster, false, Java2DUtils.getProperties(bufferedImage));
+        final BufferedImage result = ccOp.filter(bufferedImage, image2);
 
         // Strategy 3: work in place. It would be the best, but it does not work: produces a dark image.
         //        ccOp.filter(image, image);
         // Strategy 4: create an explicity destination image with the same SampleModel as the source, but produces a dark image
         //        BufferedImage image2 = createCopy2(false); // - stessa conseguenza dell'in place
         //        image = ccOp.filter(image, image2);
-        Java2DUtils.logImage(logger, ">>>> >>>> convertColorProfile() returning ", bufferedImage);
+        Java2DUtils.logImage(log, ">>>> >>>> convertColorProfile() returning ", result);
 
-        return bufferedImage;
+        return result;
       }
   }
