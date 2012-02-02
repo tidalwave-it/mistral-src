4c6e5048feabfb395dbd60ce6fcf60376935f2dc
fabriziogiudici <fabrizio dot giudici at tidalwave dot it@localhost>
2012-02-02 17:35:02 +0100

    Fixed headers.

--- runViewerExample.bat
+++ runViewerExample.bat
@@ -16,7 +16,7 @@
 @REM
 @REM **********************************************************************************************************************
 @REM
-@REM  WWW: http://mistral.tidalwave.it
+@REM  WWW: http://mistral.imajine.org
 @REM  SCM: https://bitbucket.org/tidalwave/mistral-src
 @REM
 @REM
