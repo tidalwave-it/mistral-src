b1428a8803b0c4b30264aac4bdadb02f88ad3e43
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2023-01-16 14:11:51 +0100

    Updated copyright notice.

--- runViewerExample.bat
+++ runViewerExample.bat
@@ -1,7 +1,7 @@
 @REM
 @REM
 @REM  Mistral - open source imaging engine
-@REM  Copyright (C) 2003-2012 by Tidalwave s.a.s.
+@REM  Copyright (C) 2003-2023 by Tidalwave s.a.s.
 @REM
 @REM **********************************************************************************************************************
 @REM
